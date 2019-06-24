685b44ca8f5d748b226565f769c4eff9cee32e6f
almost-hero <bgmakar79@gmail.com>
2019-06-25 00:34:25 +0300

    Create a form that adds a new user

---
+++ libmain/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class AddUser(forms.ModelForm):
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class':'form-control','type':'text','placeholder':'Username'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class':'form-control','type':'password','placeholder':'Password'
+    }))
+
+
+    class Meta:
+        model = User
+        fields = ['username','password']
+
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        username_qs = User.objects.filter(username = username)
+        if username_qs.exists():
+            raise forms.ValidationError('This username is already exist')
+        return username
--- libmain/views.py
+++ libmain/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from .forms import *
 
 # Create your views here.
 def users_list(request):
-    return render(request,'libmain/users_list.html')
+    users = User.objects.all()
+    form = AddUser(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        password = form.cleaned_data.get('password')
+        user.set_password(password)
+        user.save()
+        new_user = authenticate(username = user.username, password= password)
+    return render(request,'libmain/users_list.html',{'users':users,'form':form})
